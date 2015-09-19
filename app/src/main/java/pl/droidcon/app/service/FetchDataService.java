d7e2c08751c6473e2e1e7626a36a27ef1cfb7652
Przemek Jakubczyk <pjakubczyk@applause.com>
2015-09-19 18:43:45 +0200

    Adding Robospice as data binding service and async calls

--- app/src/main/java/pl/droidcon/app/service/FetchDataService.java
+++ app/src/main/java/pl/droidcon/app/service/FetchDataService.java
@@ -2,11 +2,6 @@ package pl.droidcon.app.service;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.widget.Toast;
-
-import pl.droidcon.app.service.api.AgendaService;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
 
 //TODO
 public class FetchDataService extends IntentService {
@@ -19,12 +14,5 @@ public class FetchDataService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        RestAdapter.Builder builder = new RestAdapter.Builder();
-        builder.setEndpoint("https://raw.githubusercontent.com/droidconpl/droidcon-2015-web/master");
-        builder.setLogLevel(RestAdapter.LogLevel.FULL);
-        RestAdapter build = builder.build();
-        AgendaService agendaService = build.create(AgendaService.class);
-
-        Toast.makeText(getApplicationContext(), agendaService.getAllSessions().toString(), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/pl/droidcon/app/ui/fragment/agenda/AgendaFragment.java
+++ app/src/main/java/pl/droidcon/app/ui/fragment/agenda/AgendaFragment.java
@@ -1,19 +1,30 @@
 package pl.droidcon.app.ui.fragment.agenda;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.DurationInMillis;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
+
+import java.util.List;
 
 import pl.droidcon.app.R;
-import pl.droidcon.app.service.FetchDataService;
+import pl.droidcon.app.service.api.AgendaRetrofitService;
+import pl.droidcon.app.service.api.AgendaRetrofitSpiceRequest;
+import pl.droidcon.app.service.model.AgendaResponse;
+import pl.droidcon.app.service.model.Session;
 
 
 public class AgendaFragment extends Fragment {
 
+    private SpiceManager spiceManager = new SpiceManager(AgendaRetrofitService.class);
 
     public static AgendaFragment newInstance() {
         Bundle args = new Bundle();
@@ -26,13 +37,49 @@ public class AgendaFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Intent msgIntent = new Intent(getContext(), FetchDataService.class);
-        getContext().startService(msgIntent);
         return inflater.inflate(R.layout.agenda_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        AgendaRetrofitSpiceRequest contactsRetrofitSpiceRequest = new AgendaRetrofitSpiceRequest();
+        spiceManager.execute(contactsRetrofitSpiceRequest, "contacts", DurationInMillis.ALWAYS_RETURNED, new ListAllSessionsListener());
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        spiceManager.start(getActivity());
+    }
+
+    @Override
+    public void onStop() {
+        if (spiceManager.isStarted()) {
+            spiceManager.shouldStop();
+        }
+        super.onStop();
+    }
+
+    public void update(List<Session> sessions) {
+        Toast.makeText(getContext(), sessions.get(0).toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    class ListAllSessionsListener implements RequestListener<AgendaResponse> {
+
+        @Override
+        public void onRequestFailure(SpiceException spiceException) {
+            Toast.makeText(getContext(), "WILL HANDLE LATER", Toast.LENGTH_SHORT);
+        }
+
+        @Override
+        public void onRequestSuccess(AgendaResponse agendaResponse) {
+            update(agendaResponse.sessions);
+        }
     }
 }
