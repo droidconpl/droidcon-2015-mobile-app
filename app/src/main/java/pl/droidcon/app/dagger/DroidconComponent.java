a78f652484236ef48e87d87f20377149f4bb413e
whiter13 <bialko1990@gmail.com>
2015-09-22 21:35:07 +0200

    New resources for http

--- app/src/main/java/pl/droidcon/app/dagger/DroidconComponent.java
+++ app/src/main/java/pl/droidcon/app/dagger/DroidconComponent.java
@@ -3,10 +3,11 @@ package pl.droidcon.app.dagger;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import pl.droidcon.app.dagger.module.ApiModule;
 import pl.droidcon.app.dagger.module.UIModule;
 
 @Singleton
-@Component(modules = {UIModule.class})
+@Component(modules = {UIModule.class, ApiModule.class})
 public interface DroidconComponent extends DroidconGraph {
 
     final class Initializer {
