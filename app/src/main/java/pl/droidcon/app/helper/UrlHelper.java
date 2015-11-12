707ceb204853fbd3d70aec1b066ceefee8d68707
whiter13 <bialko1990@gmail.com>
2015-11-12 19:30:20 +0100

    Full screen speaker photo after click on image in session details

--- app/src/main/java/pl/droidcon/app/helper/UrlHelper.java
+++ app/src/main/java/pl/droidcon/app/helper/UrlHelper.java
@@ -1,6 +1,10 @@
 package pl.droidcon.app.helper;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
 public final class UrlHelper {
 
     private static final String BASE_URL = "https://raw.githubusercontent.com/droidconpl/droidcon-2015-web/master/";
@@ -9,7 +13,14 @@ public final class UrlHelper {
 
     }
 
-    public static String url(String imgAddress){
+    public static String url(String imgAddress) {
         return BASE_URL + imgAddress;
+    }
+
+    public static void a(Context b) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri data = Uri.parse("mailto:contact@droidcon.pl?subject=Droidcon Kraków 2015" + "&body=I have found that!!!");
+        intent.setData(data);
+        b.startActivity(intent);
     }
 }
