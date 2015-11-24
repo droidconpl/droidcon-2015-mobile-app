4c99158288145316e74ff35ddfa80173b252a253
whiter13 <bialko1990@gmail.com>
2015-11-24 22:19:33 +0100

    Blocking clicks on non session elements

--- app/src/main/java/pl/droidcon/app/ui/fragment/agenda/AgendaFragment.java
+++ app/src/main/java/pl/droidcon/app/ui/fragment/agenda/AgendaFragment.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -186,6 +185,9 @@ public class AgendaFragment extends Fragment implements SwipeRefreshLayout.OnRef
     @Override
     public void onItemClick(View view, int position) {
         Session session = agendaAdapter.getSessionByPosition(position);
+        if (session.getSpeakersList().isEmpty()) {
+            return;
+        }
         SessionActivity.start(getContext(), session);
     }
 }
